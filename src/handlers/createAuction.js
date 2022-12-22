614d315599ea975a984f0dcfac5a3324fdadca57
Yury Firsau <janin90@yandex.ru>
2022-12-22 19:34:13 +0300

    next changes

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,11 +1,23 @@
+import { v4 } from 'uuid';
+import AWS from 'aws-sdk';
+
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction (event, context) {
     const { title } = JSON.parse(event.body);
     const now = new Date();
     const auction = {
+        id: v4(),
         title,
         status: 'OPEN',
         createdAt: now.toISOString()
     };
+
+    await dynamoDB.put({
+        TableName: 'auctionsTable',
+        Item: auction
+    }).promise();
 
     return {
         statusCode: 201,
